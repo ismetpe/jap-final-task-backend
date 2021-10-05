5c0a7a90e19a1bc6ab7c1bc3cb2df7e99b72cc59
ismetpe <ismetpe@maestralsolutions.com>
2021-10-05 08:38:09 +0200

    intial commit

---
+++ movie-app-task-backend/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Core.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movie_app_task_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+   
+
+            private readonly IAuthService _authService;
+            public AuthController(IAuthService authRepo)
+            {
+            _authService = authRepo;
+
+            }
+
+            [HttpPost("Register")]
+            public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegistrationDto request)
+            {
+                var response = await _authService.Register(
+                    new User { Username = request.Username }, request.Password
+                );
+
+                if (!response.Success)
+                {
+                    return BadRequest(response);
+                }
+
+                return Ok(response);
+            }
+
+            [HttpPost("Login")]
+            public async Task<ActionResult<ServiceResponse<string>>> Login(UserLoginDto request)
+            {
+                var response = await _authService.Login(
+                    request.Username, request.Password
+                );
+
+                if (!response.Success)
+                {
+                    return BadRequest(response);
+                }
+
+                return Ok(response);
+            }
+        }
+    }
+
---
+++ movie-app-task-backend/Controllers/ScreeningsController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movie_app_task_backend.Controllers
+{
+    [ApiController]
+    [Route("api/screenings")]
+    public class ScreeningsController : ControllerBase
+    {
+        private readonly IScreeningsService _screeningsService;
+
+        public ScreeningsController(IScreeningsService screeningsService)
+        {
+            _screeningsService = screeningsService;
+        }
+        [HttpPost("buy_ticket")]
+        public async Task<ActionResult<int>> BuyTicket(int UserID, int ScreeningID, int NumberOfTickets, DateTime DateOfBuying)
+        {
+            return Ok(await _screeningsService.BuyTickets( UserID,  ScreeningID,  NumberOfTickets,  DateOfBuying));
+        }
+    }
+}
